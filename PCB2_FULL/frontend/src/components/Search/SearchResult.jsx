6b3fcc2ba30fa379f93405315d3c11bbd8d7aa81
NortonFire007 <108221292+NortonFire007@users.noreply.github.com>
2024-06-21 13:13:03 +0300

    Delete PCB2_FULL/frontend directory

--- PCB2_FULL/frontend/src/components/Search/SearchResult.jsx
+++
@@ -1,5 +0,0 @@
-const SearchResult = ({ result }) => {
-  return <div className="px-2.5 py-5 hover:bg-neutral-100">{result.name}</div>;
-};
-
-export default SearchResult;
--- PCB2_FULL/frontend/src/services/CategoryService.js
+++
@@ -1,7 +0,0 @@
-import $api from '../http';
-
-export default class CategoryService {
-  static async getCategories() {
-    return $api.get('/categories');
-  }
-}
